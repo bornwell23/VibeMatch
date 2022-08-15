7a827f2500bed2b02a66adc286402d7aea8251e2
Brit Cornwell <britcornwell23@gmail.com>
2022-08-14 18:42:24 -0700

    add init file

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+The purpose of this repository is to build an automated tool to find similar songs based on input such as a spotify link\
+The similarities could include bpm, genre, keys, melodies, vocals, styles, etc
+
+Github link is: https://github.com/bornwell23/VibeMatch
+
+
+To set environment:
+* install requirements.txt (pip install -r requirements.txt)
+* if using Pycharm
+  * Open settings, search for Python Integrated Tools
+    * set default test runner to pytest
+    * set docstring format to Google
+* run tests (python -m pytest test.py)
+"""
